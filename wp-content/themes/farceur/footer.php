ba52be1dc483a026fb827931914c91df4e721544
Steve Letter <steven.w.letter@gmail.com>
2018-06-01 19:46:54 -0700

    Update footer so Guidestar graphic local instead of read from site (url)

--- wp-content/themes/farceur/footer.php
+++ wp-content/themes/farceur/footer.php
@@ -14,11 +14,11 @@
   
 	<div id="outerfooter">
 		<div id="footer"> 
-			<div> <a href="/about-us/" class="bldul">About Us </a><br /> <a href="/about-us/our-mission/">Our Mission</a> <br /> <a href="/about-us/directors/">Directors</a><br /> <a href="/about-us/our-friends/">Our Friends </a></div>
+			<div> <a href="/about-us/" class="bldul">About Us </a><br /> <a href="/about-us/our-mission/">Our Mi'ssion</a> <br /> <a href="/about-us/directors/">Directors</a><br /> <a href="/about-us/our-friends/">Our Friends </a></div>
 			<div> <a href="/calendar/" class="bldul">Events </a><br /><a href="/members/" class="bldul">Members</a></div>
 			<div> <a href="/news/" class="bldul">News</a> <br /> <a href="/projects/">Outreach </a><br /> <a href="/archives/">News Archive</a> <br /> <a href="/CCIPressKit.pdf">Press Kit</a><br /><a href="/privacy-policy/">Privacy Policy</a> </div>
 			<div> <a href="/get-involved/" class="bldul">Get Involved </a><br /> <a href="/join-us/">Join Us</a><br /> <a href="/donate/">Donate </a> <br /> <a href="http://www.volunteermatch.org/search/org211108.jsp">Volunteer</a> <br /><a href="/hire-us/"> Hire Us</a> </div>
-			<div> <a class="bldul" href="http://www.guidestar.org/profile/80-0120296" target="_blank"><img style="width:70%" src="https://widgets.guidestar.org/gximage2?o=8217924&l=v4" /></a> </div>
+			<div> <a class="bldul" href="<?php echo esc_url( get_template_directory_uri() ) . '/Images/gximage2.png'?>" target="_blank"><img style="width:70%" src="https://widgets.guidestar.org/gximage2?o=8217924&l=v4" /></a> </div>
 		</div><!--end footer div-->
 		<div class="clear"></div>
 		<div class="footerbottom">
