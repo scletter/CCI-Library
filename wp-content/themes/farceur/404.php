2da75f5bed03589fb640146ced94a5000c4b9316
Steve Letter <steven.w.letter@gmail.com>
2018-06-06 15:16:50 -0700

    Fixed a couple theme-check errors including adding a screenshot for theme.

--- wp-content/themes/farceur/404.php
+++ wp-content/themes/farceur/404.php
@@ -4,15 +4,15 @@
  *
  * @package WordPress
  * @subpackage farceur
- * @since farceur 1.0.0
+ * @since farceur 1.1.0
  */
 
 get_header(); ?>
  <img src="/Images/PageTitle_NotFound.jpg" class="pagetitle" /> 
 
 
-				<h1><?php _e( 'Not Found', 'starkers' ); ?></h1>
-				<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'starkers' ); ?></p>
+				<h1><?php _e( 'Not Found', 'farceur' ); ?></h1>
+				<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'farceur' ); ?></p>
 				<?php get_search_form(); ?>
 
 	<script type="text/javascript">
