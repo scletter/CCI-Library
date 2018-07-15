bb8e2b6a7b05f66fb283cd45fc2ea8dd9462242e
Steve Letter <steven.w.letter@gmail.com>
2018-07-14 18:41:28 -0700

    Corrected the Attachment Template which was never modified from the original TwentyTen template and didn't display correctly if there were attachments (which would never fire with the shadowbox

--- wp-content/themes/farceur/attachment.php
+++ wp-content/themes/farceur/attachment.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying attachments.
+ * The template for displaying Attachment pages.
  *
  * @package WordPress
  * @subpackage farceur
@@ -8,91 +8,130 @@
  */
 
 get_header(); ?>
+<div id="attachmentwrap">
+	<div id="attachmentcontent">
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<h1><?php printf( __( 'Attachment: %s', 'farceur' ), '' . single_cat_title( '', false ) . '' . get_the_title( $post->post_parent ) );	?></h1>
+		<br /><br />
 
-				<p><a href="<?php echo get_permalink( $post->post_parent ); ?>" title="<?php esc_attr( printf( __( 'Return to %s', 'farceur' ), get_the_title( $post->post_parent ) ) ); ?>" rel="gallery"><?php
-					/* translators: %s - title of parent post */
-					printf( __( '<span>&laquo;</span> %s', 'farceur' ), get_the_title( $post->post_parent ) );
-				?></a></p>
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-					<h2><?php the_title(); ?></h2>
 
-						<?php
-							printf(__('By %2$s', 'farceur'),
-								'meta-prep meta-prep-author',
-								sprintf( '<a class="url fn n" href="%1$s" title="%2$s">%3$s</a>',
-									get_author_posts_url( get_the_author_meta( 'ID' ) ),
-									sprintf( esc_attr__( 'View all posts by %s', 'farceur' ), get_the_author() ),
-									get_the_author()
-								)
-							);
-						?>
-						<span>|</span>
-						<?php
-							printf( __('Published %2$s', 'farceur'),
-								'meta-prep meta-prep-entry-date',
-								sprintf( '<abbr title="%1$s">%2$s</abbr>',
-									esc_attr( get_the_time() ),
-									get_the_date()
-								)
-							);
-							if ( wp_attachment_is_image() ) {
-								echo ' | ';
-								$metadata = wp_get_attachment_metadata();
-								printf( __( 'Full size is %s pixels', 'farceur'),
-									sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
-										wp_get_attachment_url(),
-										esc_attr( __('Link to full-size image', 'farceur') ),
-										$metadata['width'],
-										$metadata['height']
-									)
-								);
-							}
-						?>
-						<?php edit_post_link( __( 'Edit', 'farceur' ), '', '' ); ?>
-					</div><!-- .entry-meta -->
+		<h2><?php the_title(); ?></h2>
 
-<?php if ( wp_attachment_is_image() ) :
-	$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
-	foreach ( $attachments as $k => $attachment ) {
-		if ( $attachment->ID == $post->ID )
-			break;
-	}
-	$k++;
-	// If there is more than 1 image attachment in a gallery
-	if ( count( $attachments ) > 1 ) {
-		if ( isset( $attachments[ $k ] ) )
-			// get the URL of the next image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-		else
-			// or get the URL of the first image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-	} else {
-		// or, if there's only 1 image attachment, get the URL of the image
-		$next_attachment_url = wp_get_attachment_url();
-	}
-?>
-						<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
-							$attachment_size = apply_filters( 'farceur_attachment_size', 900 );
-							echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); // filterable image width with, essentially, no limit for image height.
-						?></a></p>
+			<div id="postedon">
+			<?php
+				printf(__('By %2$s', 'farceur'),
+					'meta-prep meta-prep-author',
+					sprintf( '<a href="%1$s" title="%2$s">%3$s</a>',
+						get_author_posts_url( get_the_author_meta( 'ID' ) ),
+						sprintf( esc_attr__( 'View all posts by %s', 'farceur' ), get_the_author() ),
+						get_the_author()
+					)
+				);
+			?>
 
-							<?php previous_image_link( false ); ?>
-							<?php next_image_link( false ); ?>
-<?php else : ?>
-						<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php echo basename( get_permalink() ); ?></a>
-<?php endif; ?>
-						<?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?>
+			<span>|</span>
+			<?php
+				printf( __('Published %2$s', 'farceur'),
+					'meta-prep meta-prep-entry-date',
+					sprintf( '<abbr title="%1$s">%2$s</abbr>',
+						esc_attr( get_the_time() ),
+						get_the_date()
+					)
+				);
+				if ( wp_attachment_is_image() ) {
+					echo ' | ';
+					$metadata = wp_get_attachment_metadata();
+					printf( __( 'Full size is %s pixels', 'farceur'),
+						sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
+							wp_get_attachment_url(),
+							esc_attr( __('Link to full-size image', 'farceur') ),
+							$metadata['width'],
+							$metadata['height']
+						)
+					);
+				}
+			?>
+			</div>
 
-<?php the_content( __( 'Continue reading &raquo;', 'farceur' ) ); ?>
-<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'farceur' ), 'after' => '' ) ); ?>
+		<?php if ( wp_attachment_is_image() ) :
+					$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
+					foreach ( $attachments as $k => $attachment ) {
+						if ( $attachment->ID == $post->ID )
+							break;
+					}
+					$k++;
+					
+					// If there is more than 1 image attachment in a gallery
+					if ( count( $attachments ) > 1 ) {
+						if ( isset( $attachments[ $k ] ) )
+							// get the URL of the next image attachment
+							$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
+						else
+							// or get the URL of the first image attachment
+							$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
+					}
+					else {
+					// or, if there's only 1 image attachment, get the URL of the image
+						$next_attachment_url = wp_get_attachment_url();
+					}
+				?>
+					<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
+						$attachment_size = apply_filters( 'farceur_attachment_size', 900 );
+						echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); // filterable image width with, essentially, no limit for image height.
+					?></a>
+					</p>
 
-						<?php farceur_posted_in(); ?>
-						<?php edit_post_link( __( 'Edit', 'farceur' ), ' ', '' ); ?>
+					<?php // display previous and next image link //?>
+					<?php printf ( '&laquo; Previous:' . ' '); ?>
+					<?php  previous_image_link( false ); ?>
+					
+					<?php printf ( ' | ' . 'Next: ' . ' '); ?>
+					<?php  next_image_link( false ); ?>
+					<?php printf ( ' &raquo;'); ?>
+					
+ 
+		<?php
+		// Attachment is not an image
+		else :
+		?>
+			<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php echo basename( get_permalink() ); ?></a>
+		<?php 
+		endif; ?>
+			
+		<?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?>
 
-<?php comments_template(); ?>
+		<?php the_content( __( 'Continue reading &raquo;', 'farceur' ) ); ?>
+
+		<?php
+			$defaults = array(
+				'before'           => '<p>' . __( 'Pages:', 'farceur' ),
+				'after'            => '</p>',
+				'link_before'      => '',
+				'link_after'       => '',
+				'next_or_number'   => 'number',
+				'separator'        => ' ',
+				'nextpagelink'     => __( 'Next page', 'farceur'),
+				'previouspagelink' => __( 'Previous page', 'farceur' ),
+				'pagelink'         => '%',
+				'echo'             => 1
+			);
+ 
+			wp_link_pages( $defaults );
+
+		?>
+		
+		<br/>
+		<?php // farceur_posted_in(); ?>
+		
+		<?php comments_template(); ?>
 
 <?php endwhile; ?>
-
+				
+				
+	</div>
+	<?php get_sidebar(); ?>
+	
+</div> <!-- catwrap -->
 <?php get_footer(); ?>
