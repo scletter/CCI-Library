340ffcf3ac5161542d8b97874098cea391b5a70b
Steve Letter <steven.w.letter@gmail.com>
2018-07-21 20:43:09 -0700

    Fixed Peru 2011 template to display all posts of this category. Pagination functions don't work if outside the main loop.  Also modified template to include sidebar to have it consistent with the e other special page posts.

--- wp-content/themes/farceur/Peru2011.php
+++ wp-content/themes/farceur/Peru2011.php
@@ -5,55 +5,60 @@
  *
  * @package WordPress
  * @subpackage farceur
- * @since farceur 1.0.0
+ * @since farceur 1.3.0
  */
 ?>
 
 <?php get_header(); ?><img src="<?php $key="PageTitleImage"; echo get_post_meta($post->ID, $key, true); ?>" class="pagetitle" />
 
-<div id="peru2011posts">
+<div id="peruwrap">
+	<div id="peruposts">
 
-<?php 
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$args=array(
-   'cat'=>4,
-   'posts_per_page'=>10,
-   'ignore_sticky_posts'=>1,
-   'paged'=>$paged,
-   );
-?>
-
-	<?php
-	// Query the loop for Peru (Category 7) for the current page
-	$my_query = new WP_Query( $args );
+	<?php 
+	// Set up to display all posts for Peru 2011.  No pagination. 
+	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+	$args=array(
+	   'cat'=>4,
+	   'posts_per_page'=>-1,
+	   'ignore_sticky_posts'=>1,
+	   'paged'=>$paged,
+	   );
 	?>
 
-<?php if ( $my_query->have_posts() ) : while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+		<?php
+		// Query the loop for Peru (Category 4) for the current page
+		$my_query = new WP_Query( $args );
+		?>
 
-<div class="perupost" id="post-<?php the_ID(); ?>">
+	<?php if ( $my_query->have_posts() ) : while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 
-    <div class="posttitle">
-        <div id="posttitlecontent">   		
-            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> 
-        </div>      
-                
-    </div>  
-    <div id="perucontent"><?php the_content('&raquo;&raquo; Read More'); ?></div> 
+	<div class="perupost" id="post-<?php the_ID(); ?>">
 
-	<?php farceur_post_meta(); ?>	
-	<br />
-	
+		<div class="posttitle">
+			<div id="posttitlecontent">   		
+				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a> 
+			</div>      
+					
+		</div>  
+		<div id="perucontent"><?php the_content('&raquo;&raquo; Read More'); ?></div> 
+
+		<?php farceur_post_meta(); ?>	
+		<br />
+		
+	</div>
+	<div class="clear"></div> 
+
+	<?php endwhile; else: ?>
+	<p><?php _e('Sorry, no posts matched your criteria.','farceur'); ?></p>
+	<?php endif; ?>
+
+		<?php next_posts_link( __( '&laquo; Older posts', 'farceur' ) ); ?>
+		<?php previous_posts_link( __( '| Newer posts &raquo;', 'farceur' ) ); ?>
+
+	</div>
+
+	<?php get_sidebar(); ?>
+
 </div>
-<div class="clear"></div> 
-
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.','farceur'); ?></p>
-<?php endif; ?>
-
-	<?php next_posts_link( __( '&laquo; Older posts', 'farceur' ) ); ?>
-	<?php previous_posts_link( __( '| Newer posts &raquo;', 'farceur' ) ); ?>
-
-</div>
-
 <?php get_footer(); ?>
 
